1b671f94d27e258f2a2f7fb068fe2dbce271f2a1
phuongthang942005 <>
2021-11-20 23:46:39 +0700

    Update script

--- test/pageObjects/page.js
+++ test/pageObjects/page.js
@@ -3,12 +3,13 @@ module.exports = class Page {
     get listProduct () { return $$("//div[starts-with(@class,'MuiContainer-root')]/div//a//img")}
 
     get lbl_CheckResult () { return $('//div[@data-test-check-result]/p') }
+
     /**
     * Opens a sub page of the page
     * @param path path of the sub page (e.g. /path/to/page.html)
     */
     open (path) {
-        browser.url(`https://element-challenge.flood.io/${path}`)
+        browser.url(`/${path}`)
         browser.maximizeWindow()
     }
 
